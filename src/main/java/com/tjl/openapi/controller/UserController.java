fcef4930c28e9f4fc26528e457a2bf638647225d
唐三石 <2770184053@qq.com>
2024-04-27 16:22:26 +0800

    init

---
+++ src/main/java/com/tjl/openapi/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.tjl.openapi.controller;
+
+import co.elastic.clients.elasticsearch.core.DeleteRequest;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import com.tjl.openapi.common.ErrorCode;
+import com.tjl.openapi.common.R;
+import com.tjl.openapi.common.ResultUtils;
+import com.tjl.openapi.exception.BusinessException;
+import com.tjl.openapi.exception.ThrowUtils;
+import com.tjl.openapi.model.User;
+import com.tjl.openapi.model.vo.UserReg;
+import com.tjl.openapi.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户接口
+ */
+@RestController
+@RequestMapping("/user")
+@Slf4j
+@Tag(name = "用户接口")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/register")
+    public R<Boolean> register(@RequestBody UserReg regUser) {
+        if (StringUtils.isAllBlank(regUser.getUserAccount(),
+                regUser.getUserPassword(),regUser.getCheckPassword())){
+            ThrowUtils.paramsError("帐号或密码不能为空");
+        }
+        if (!regUser.getUserPassword().equals(regUser.getCheckPassword())){
+            ThrowUtils.paramsError("密码不一致");
+        }
+        User user = new User();
+        BeanUtils.copyProperties(regUser, user);
+        int register = userService.register(user);
+        return ResultUtils.success(register > 0);
+    }
+
+    @PostMapping("/login")
+    public R<User> login(@RequestBody UserReg loginUser){
+        if (StringUtils.isAllBlank(loginUser.getUserAccount(),
+                loginUser.getUserPassword())){
+            ThrowUtils.paramsError("帐号或密码不能为空");
+        }
+        User user = new User();
+        BeanUtils.copyProperties(loginUser, user);
+        return ResultUtils.success(userService.login(user));
+    }
+
+    @PostMapping("/ex")
+    public R<String> ex() {
+        throw new BusinessException(ErrorCode.SYSTEM_ERROR,"业务异常");
+    }
+}
---
+++ src/main/java/com/tjl/openapi/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.tjl.openapi.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tjl.openapi.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 27701
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-04-27 14:23:44
+* @Entity com.tjl.openapi.model.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+
+}
+
+
+
+
